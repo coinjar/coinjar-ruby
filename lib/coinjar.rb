ceaca952059bf68d46e71df9ad8fa429c8dc0eea
thoran <code@thoran.com>
2014-07-04 09:31:49 +1000

    Added a bunch of instance and class methods to CoinJar::Transaction to assist with separating out the different transaction types and some convenience methods on CoinJar which delegate to those methods, and also added a couple of methods to find transactions by their bitcoin txid.

--- lib/coinjar.rb
+++ lib/coinjar.rb
@@ -12,12 +12,42 @@ require 'coinjar/fair_rate'
 
 module CoinJar
   extend Configuration
-  
+
   class << self
     def client
       @client = CoinJar::Client.new unless defined?(@client) && @client.hash == config_options.hash
       @client
     end
-  end
-  
+
+    def all_transactions
+      Transaction.all
+    end
+    alias_method :transactions, :all_transactions
+
+    def bitcoin_transactions
+      Transaction.bitcoin_transactions
+    end
+
+    def exchange_transactions
+      Transaction.exchange_transactions
+    end
+
+    def bitcoin_deposits
+      Transaction.bitcoin_deposits
+    end
+
+    def bitcoin_withdrawals
+      Transaction.bitcoin_withdrawals
+    end
+
+    def purchases
+      Transaction.purchases
+    end
+
+    def sales
+      Transaction.sales
+    end
+
+  end # class << self
+
 end
--- lib/coinjar/transaction.rb
+++ lib/coinjar/transaction.rb
@@ -1,33 +1,111 @@
 module CoinJar
   class Transaction
-    
+
     attr_accessor :uuid, :counterparty_name, :counterparty_type, :amount, :status, :confirmations, :bitcoin_txid, :related_payment_uuid, :created_at, :updated_at
-    
+
     def initialize(args)
       reset(args)
     end
-    
+
     def fetch
       response = CoinJar.client.get("transactions/" + uuid)
       self.reset(response[:transaction])
       self
     end
-    
+
     def self.find(uuid)
       transaction = self.new(uuid: uuid)
       transaction.fetch
       transaction
     end
-    
+
     def self.list(offset = 0, limit = 100)
       CoinJar.client.get("transactions", { offset: offset, limit: limit })[:transactions].map { |t| self.new t }
     end
-    
+
     def reset(args)
       args.each do |k,v|
         instance_variable_set("@#{k}", v) unless v.nil?
       end
     end
-    
+
+    def self.all
+      offset = -100
+      all_transactions = []
+      query_results = [nil]
+      until query_results.empty?
+        query_results = self.list(offset += 100)
+        all_transactions += query_results
+      end
+      all_transactions
+    end
+
+    def self.bitcoin_transactions
+      all.select{|t| t.bitcoin_transaction?}
+    end
+
+    def self.exchange_transactions
+      all.select{|t| t.exchange_transaction?}
+    end
+
+    def self.bitcoin_deposits
+      bitcoin_transactions.select{|t| t.deposit?}
+    end
+
+    def self.bitcoin_withdrawals
+      bitcoin_transactions.select{|t| t.withdrawal?}
+    end
+
+    def self.purchases
+      all.select{|t| t.purchase?}
+    end
+
+    def self.sales
+      all.select{|t| t.sale?}
+    end
+
+    def self.find_by_txid(txid)
+      bitcoin_transactions.detect{|t| txid == t.bitcoin_txid}
+    end
+
+    def self.find_all_by_txid(*txids)
+      txids = txids.flatten
+      bitcoin_transactions.select{|t| txids.include?(t.bitcoin_txid)}
+    end
+
+    # boolean methods
+
+    def bitcoin_transaction?
+      bitcoin_txid
+    end
+
+    def exchange_transaction?
+      bitcoin_txid.nil?
+    end
+
+    def deposit?
+      amount.to_f > 0
+    end
+
+    def withdrawal?
+      amount.to_f < 0
+    end
+
+    def bitcoin_deposit?
+      bitcoin_transaction? && deposit?
+    end
+
+    def bitcoin_withdrawal?
+      bitcoin_transaction? && withdrawal?
+    end
+
+    def purchase?
+      exchange_transaction? && deposit? # Effectively a deposit of bitcoin from fiat.
+    end
+
+    def sale?
+      exchange_transaction? && withdrawal? # Effectively a withdrawal of bitcoin to fiat.
+    end
+
   end
 end
